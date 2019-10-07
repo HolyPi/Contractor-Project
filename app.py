c23246f4d53c472284fa1cc2cbafce3422d20cdd
HolyPi <joanelly2018@gmail.com>
2019-10-07 00:32:48 -0700

    Added Mongodb

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Playlister
+playlists = db.playlists
 
 app = Flask(__name__)
 
-
-videogame = [
-
-            { 'title': 'Fire Emblem Three houses' },
-            { 'price': '59.99' }
-]
+#
+# videogame = [
+#
+#             { 'title': 'Fire Emblem Three houses' },
+#             { 'price': '59.99' },
+#             { 'title': 'Danganronpa V3'},
+#             { 'price': '59.99' }
+# ]
 
 
 @app.route('/')
-def index():
-    """Return homepage."""
-    return render_template('home.html')
-
-@app.route('/videogame')
 def videogame_index():
     "Shows videogames"
-    return render_template('videogame_index.html', videogame=videogame)
+    return render_template('videogame_index.html', videogame=videogame.find()
 
 
 if __name__ == '__main__':
