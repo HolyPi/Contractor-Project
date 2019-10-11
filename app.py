626a0da7460376ba9ce01274d65bf97e2c33aa3c
HolyPi <joanelly2018@gmail.com>
2019-10-10 20:11:22 -0700

    Comments

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
 from bson.objectid import ObjectId
@@ -7,8 +7,10 @@ app = Flask(__name__)
 client = MongoClient()
 db = client.get_default_database('GameMania')
 videogames = db.videogames
+comments = db.comments
 cart = db.cart
 cart.delete_many({})
+
 
 videogames.delete_many({})
 videogames.insert_many(
@@ -26,13 +28,14 @@ videogames.insert_many(
 @app.route('/')
 def videogame_index():
     """Show all videogames."""
-    return render_template('videogame_index.html', videogame=videogames.find())
+    return render_template('videogame_index.html', videogames=videogames.find())
 
 @app.route('/videogame/<videogame_id>', methods=['GET'])
 def videogame_show(videogame_id):
     "Individual Videogame"
     videogame = videogames.find_one({'_id': ObjectId(videogame_id)})
-    return render_template('videogame_show.html', videogame=videogame)
+    videogame_comments = comments.find({'videogame_id': ObjectId(videogame_id)})
+    return render_template('videogame_show.html', videogame=videogame, comment=videogame_comments)
 
 
 
@@ -68,17 +71,51 @@ def remove_from_cart(videogame_id):
     cart.delete_one({'_id':ObjectId(videogame_id)})
     return redirect(url_for('view_cart'))
 
-@app.route("/checkout/purchase")
-def view_purchase():
-    cart = 0
-    return render_template('purchase.html')
+@app.route("/<videogame_id>/checkout")
+def view_checkout(videogame_id):
+    "Purchase screen"
+    cart.delete_many({'_id':ObjectId(videogame_id)})
+    return render_template("purchase.html")
 
-@app.route("/checkout")
-def checkout_cart(videogame_id):
-    if cart.count_documents({}) <= 0:
-        Nothing = "You currently have nothing inside your cart"
-    return render(url_for('view_cart'))
+@app.route('/videogame/comments', methods=['POST'])
+def comments_new():
+    """Submit a new comment."""
+    comment = {
+        'title':request.form.get('title'),
+        'content':request.form.get('content'),
+        'videogame_id': ObjectId(request.form.get('videogame_id'))
+    }
+    print(comment)
+    comment_id = comments.insert_one(comment).inserted_id
+    return redirect(url_for('videogame_show', videogame_id=request.form.get('videogame_id')))
 
-    if videogame in cart.find():
-        total > 0
-    return redirect(url_for('view_purchase'))
+@app.route('/videogame/comments/<comment_id>', methods=['POST'])
+def comments_delete(comment_id):
+    """Action to delete a comment."""
+    comment = comments.find_one({'_id': ObjectId(comment_id)})
+    comments.delete_one({'_id': ObjectId(comment_id)})
+    return redirect(url_for('videogame_show', videogame_id=comment.get('videogame_id')))
+
+@app.route('/videogame/comments/<comment_id>/edit')
+def comment_edit(comment_id):
+    "Edits comments"
+    comment = comments.find_one({'_id': ObjectId(comment_id)})
+    return render_template('comment_edit.html', comment=comment)
+
+@app.route('/videogame/comments/<comment_id>', methods=['POST'])
+def comment_update(comment_id):
+    """Submit an edited comment."""
+    updated_comment = {
+        'title': request.form.get('title'),
+        'content': request.form.get('content'),
+        }
+    comment.update_one(
+        {'_id': ObjectId(comment_id)},
+        {'$set': updated_comment})
+    return redirect(url_for('videogame_show', comment_id=comment_id))
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
